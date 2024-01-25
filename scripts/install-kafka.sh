5733b9f7e5fc61c9290de03cd5287e3631fb4abc
wilhelmagren <wilhelmagren98@gmail.com>
2024-01-25 22:03:03 +0100

    fix(kafka): typo

--- scripts/install-kafka.sh
+++ scripts/install-kafka.sh
@@ -4,4 +4,3 @@ sudo adduser kafka
 sudo usermod -aG sudo kafka
 
 su - kafka bash -c 'mkdir ~/Downloads; curl "https://downloads.apache.org/kafka/3.6.1/kafka_2.13-3.6.1.tgz" -o ~/Downloads/kafka.tgz; mkdir ~/kafka && cd ~/kafka; tar -xvzf ~/Downloads/kafka.tgz --strip 1'
-
