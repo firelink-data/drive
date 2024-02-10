d1bcabcf27639bbb616e5ee5133bc49cfbbe3d4a
wilhelmagren <wilhelmagren98@gmail.com>
2024-02-10 09:56:08 +0100

    fix(master): new agent template structure

--- src/drive/templates/master/src/master.py
+++ src/drive/templates/master/src/master.py
@@ -1,5 +1,38 @@
-from confluent_kafka import Message
+"""
+MIT License
+
+Copyright (c) 2024 Firelink Data
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+File created: 2024-02-08
+Last updated: 2024-02-10
+"""
+
+import logging
 from drive.agent import Agent
+from drive.client import OpenAIClient
+
+log = logging.getLogger(__name__)
+
+# This needs config!
+client = OpenAIClient()
 
 
 class MasterAgent(Agent):
@@ -8,7 +41,11 @@ class MasterAgent(Agent):
     def __init__(self, *args, **kwargs):
         super(MasterAgent, self).__init__("Master", *args, **kwargs)
 
-    def handle_message(self, msg: Message):
+    def user_master_query(self, msg: str):
+        """ """
+        pass
+
+    def master_searcher_response(self, msg: str):
         """ """
         pass
 
